6869ff16ef4f89bdacb0362b88b6dfe1c6f5a4a5
david-the-droid <mayalsdavid@gmail.com>
2023-01-13 08:50:27 +0000

    Creating branch for automating python unit tests

--- python_pc_app/tests/test_get_pc_usage_stats.py
+++ python_pc_app/tests/test_get_pc_usage_stats.py
@@ -2,7 +2,6 @@ import unittest
 import pandas
 import get_pc_usage_stats
 
-
 class TestGetPcUsageStats(unittest.TestCase):
     def test_get_pc_dataframe(self):
         pandas_compareframe = pandas.read_csv("OpenHardwareMonitorLog-2022-09-17_new.csv")
